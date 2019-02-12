ee942fdecedf14304a441328d203d344de73d1c2
Reimen123 <shkipper92@mail.ru>
2019-02-13 00:18:26 +0200

    Feature 1 - Creating following things 1. Необходимо добавить в класс Product поля:
    категория
    скидка
    описание
    2. Необходимо дополнить проверку продукта:
    
    цена продукта должна быть больше 0
    скидка не может быть больше 100 процентов
    название не может быть меньше 3 символов и больше 32

--- src/com/javaguru/shoppinglist/CreateProductAction.java
+++ src/com/javaguru/shoppinglist/CreateProductAction.java
@@ -18,9 +18,10 @@ public class CreateProductAction implements Action {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter product name:");
         String name = scanner.nextLine();
+        checkName(name);
         System.out.println("Enter product price: ");
         String price = scanner.nextLine();
-
+        checkPrise(price);
         Product product = new Product();
         product.setName(name);
         product.setPrice(new BigDecimal(price));
@@ -37,4 +38,57 @@ public class CreateProductAction implements Action {
     public String toString() {
         return ACTION_NAME;
     }
+
+    private void checkPrise(String prise) {
+        Scanner scanner = new Scanner(System.in);
+        if(!isPositiveInteger(prise)) {
+            System.out.println("Error, Prise cant be negative ");
+            scanner.nextLine();
+        }
+    }
+
+     private boolean isPositiveInteger(String prise) {
+        if (prise == null) {
+            return false;
+        }
+        int length = prise.length();
+        if (length == 0) {
+            return false;
+        }
+        if (prise.charAt(0) == '-') {
+            return false;
+        }
+        for (int i = 0; i < length; i++) {
+            char c = prise.charAt(i);
+            boolean isDigit = (c >= '0' && c <= '9' || c == ',' || c == '.' );
+            if (!isDigit) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void checkDiscount(String prise) {
+        Scanner scanner = new Scanner(System.in);
+        long checkAmount = prise.chars().sum();
+        if (checkAmount > 100) {
+            System.out.println("Error, discount cant be more than 100%");
+            scanner.nextLine();
+        }
+    }
+
+    private void checkName(String name) {
+        Scanner scanner = new Scanner(System.in);
+        long lettersCount = name.chars().count();
+        if (lettersCount < 3) {
+            System.out.println("Error, name is too small, please try another one");
+            scanner.nextLine();
+        } else if (lettersCount > 32) {
+            System.out.println("Error, name is too big, please try another one");
+            scanner.nextLine();
+        }
+    }
+
+
 }
+
--- src/com/javaguru/shoppinglist/Product.java
+++ src/com/javaguru/shoppinglist/Product.java
@@ -8,6 +8,33 @@ public class Product {
     private Long id;
     private String name;
     private BigDecimal price;
+    private String category;
+    private BigDecimal discount;
+    private String description;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public Long getId() {
         return id;
